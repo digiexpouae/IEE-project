15a44e9289585faf0000146b66dc8e83cf0af656
ramsha1993 <rimshasaleem1993@gmail.com>
2025-08-19 16:54:43 +0500

    header fixed

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -130,7 +130,7 @@ const Header = () => {
       
       {/* Mobile Menu - Sidebar */}
       <div 
-        className={`fixed top-0 right-0 h-full w-4/5 max-w-sm bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 lg:hidden ${
+        className={`fixed top-0 right-0 h-screen w-4/5 max-w-sm bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 lg:hidden ${
           isMenuOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
