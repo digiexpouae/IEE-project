506157cec896f9f4bba94e000d4e04bdf4248b23
ramsha1993 <rimshasaleem1993@gmail.com>
2025-08-20 11:06:45 +0500

    changes done

--- src/pages/Home/section1/section1.jsx
+++ src/pages/Home/section1/section1.jsx
@@ -9,21 +9,24 @@ const Section1 = () => {
                 src={im1} 
                 alt="hero image" 
                 fill 
-                className="object-cover"
+                className="object-cover object-[center_30%]"
                 priority
             />
-            <div className="absolute inset-0 bg-[#00000066] z-[10]">
-                <div className="container max-w-4xl h-full flex flex-col items-center justify-center px-4 text-white">
+            <div className="relative h-[20vh]  md:h-[23vh]">
+            <Image src='/assets/section1/s-1-background.svg' alt="hero image" fill className="object-cover" priority/></div>
+            <div className="absolute inset-0 bg-[#00000066] z-[10] mt-10">
+                <div className="container max-w-3xl h-full flex flex-col items-center justify-center px-4 text-white">
                     <div className="max-w-xl">
+                        <div className="flex items-center"><Image src='/assets/section1/s-1.svg' width={16} height={16} /> <span className="text-xs mr-3">Truth, justice, hope</span> <Image src='/assets/section1/s-1.svg' width={16} height={16} /></div>
                         <h1 className="text-4xl md:text-6xl font-light mb-6 tracking-tight leading-tight">
                         Do you feel like<br />
                         you are lost?
                         </h1>
-                        <p className="text-xl mb-8 opacity-90 tracking-tight font-light">
+                        <span className="text-md mb-8 opacity-90 tracking-tight" >
                         The sad people in the universe are so many .. but in <br />
                         Islam you move from happiness to happiness …
-                        </p>
-                        <div className="flex flex-col sm:flex-row gap-4">
+                        </span>
+                        <div className="flex flex-col sm:flex-row gap-4 mt-4">
                             <button className=" bg-[#0A5B21] hover:bg-[#0A5B21] text-white px-8 py-3 rounded-md font-medium transition-colors">
 Welcome                            </button>
                             <button className="flex items-center justify-center gap-2 bg-[#DBAC42] hover:bg-[#DBAC42] text-black px-8 py-3 rounded-md font-medium transition-colors backdrop-blur-sm">
